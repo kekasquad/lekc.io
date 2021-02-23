91413ea79eee79622c2634cb1cd6968c743eb924
Ivan Angelov <angelow.van@yandex.ru>
2021-02-23 21:44:02 +0300

    Added form controls

--- frontend/src/Auth/Auth.tsx
+++ frontend/src/Auth/Auth.tsx
@@ -6,7 +6,11 @@ interface IProps {
 }
 
 interface IState {
-    loginMode: boolean;
+    loginMode?: boolean;
+    login?: string,
+    password?: string
+    name?: string,
+    repeatPassword?: string
 }
 
 
@@ -15,55 +19,89 @@ export default class Auth extends React.Component<IProps, IState> {
     constructor(props: any) {
         super(props);
         this.state = {
-            loginMode: props.loginMode === false ? false : true
+            loginMode: props.loginMode === false ? false : true,
+            login: '',
+            name: '',
+            password: '',
+            repeatPassword: ''
         };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
-    changeMode(loginMode: boolean) {
+    flushControls(): void {
+        this.setState(
+            { login: '', name: '', password: '', repeatPassword: '' }
+        );
+    }
+
+    changeMode(loginMode: boolean): void {
+        if (this.state.loginMode === loginMode) { return; }
         this.setState({ loginMode });
+        this.flushControls();
     }
 
-    loginForm() {
+    handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
+        this.setState(
+            { [event.target.name]: event.target.value }
+        );
+    }
+
+    loginForm(): JSX.Element {
         return (
             <form className='Auth-form'>
                 <div className="Auth-form_field">
                     <label>Login*</label>
-                    <input type='text' required/>
+                    <input
+                        type='text' required name='login'
+                        value={this.state.login}
+                        onChange={this.handleInputChange}/>
                 </div>
                 <div className="Auth-form_field">
                     <label>Password*</label>
-                    <input type='password' required/>
+                    <input
+                        type='password' required name='password'
+                        value={this.state.password}
+                        onChange={this.handleInputChange}/>
                 </div>
                 <button className='Auth-form_submit_button common_button'>Login</button>
             </form>
         );
     }
 
-    signupForm() {
+    signupForm(): JSX.Element {
         return (
             <form className='Auth-form'>
                 <div className="Auth-form_field">
                     <label>Login*</label>
-                    <input type='text' required/>
+                    <input type='text' required name='login'
+                        value={this.state.login}
+                        onChange={this.handleInputChange}/>
                 </div>
                 <div className="Auth-form_field">
                     <label>Name</label>
-                    <input type='text'/>
+                    <input type='text' name='name'
+                        value={this.state.name}
+                        onChange={this.handleInputChange}/>
                 </div>
                 <div className="Auth-form_field">
                     <label>Password*</label>
-                    <input type='password' required/>
+                    <input type='password' required name='password'
+                        value={this.state.password}
+                        onChange={this.handleInputChange}/>
                 </div>
                 <div className="Auth-form_field">
                     <label>Repeat password*</label>
-                    <input type='password' required/>
+                    <input type='password' required name='repeatPassword'
+                        value={this.state.repeatPassword}
+                        onChange={this.handleInputChange}/>
                 </div>
                 <button className='Auth-form_submit_button common_button'>Sign up</button>
             </form>
         );
     }
 
-    render() {
+    render(): JSX.Element {
       return (
         <div className='Auth-component'>
           <h1>Lekc.io</h1>
