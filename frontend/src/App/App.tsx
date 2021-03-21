c2d69b8ab1650cd7bd12840cf5f23b07fa7f31b4
Anton <leeeloooda@gmail.com>
2021-03-21 16:08:55 +0300

    add navbar

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Auth from '../Auth/Auth';
+import NavBar from '../NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Auth/>
+      <NavBar currentItem={1}/>
     </div>
   );
 }
