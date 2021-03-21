c95988ba85bf19df75d0b4351732d52a24100cfe
Anton <leeeloooda@gmail.com>
2021-03-21 21:53:10 +0300

    add routes

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -2,13 +2,28 @@ import React from 'react';
 import './App.css';
 import Auth from '../Auth/Auth';
 import NavBar from '../NavBar/NavBar';
+import { Redirect, Route, Switch, withRouter } from 'react-router';
 
 function App() {
   return (
     <div className="App">
-      <NavBar currentItem={1}/>
-    </div>
+      <Switch>
+        <Route path="/search">
+          <NavBar currentItem={0}/>
+        </Route>
+        <Route path="/stream">
+          <NavBar currentItem={1}/>
+        </Route>
+        <Route path="/profile">
+          <NavBar currentItem={2}/>
+        </Route>
+        <Route path="/login">
+          <Auth/>
+        </Route>
+        <Redirect from="/" to="/search"/>
+      </Switch>
+      </div>
   );
 }
 
-export default App;
+export default withRouter(App);
