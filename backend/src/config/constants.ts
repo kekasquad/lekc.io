652a36689f284255c307efb41dace2e510548ca2
Ivan Angelov <angelow.van@yandex.ru>
2021-04-25 01:32:11 +0300

    Removed redis and unneeded packages

--- backend/src/config/constants.ts
+++ backend/src/config/constants.ts
@@ -3,10 +3,6 @@ import path from 'path';
 export const PORT = process.env.PORT || 4000;
 export const certDir = path.resolve('cert.dev');
 export const wsUri = 'ws://localhost:8888/kurento';
-export const redisConfig = {
-    url: 'redis://localhost:6379/',
-    secret: 'kappakeepo'
-};
 export const mongoUri = 'mongodb://lekcio:s3cret_mongo@localhost:27017/lekcio';
 export const jwtConfig = {
     secret: 'Gszh0e4MCCseLY0umcmlWE32GcymUUKExKrNTAnejmHwbg4NaOU8oIs+x+PWDdBc',
