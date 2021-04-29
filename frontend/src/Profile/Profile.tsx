f8c07f17f85614d5b5d8b5ebdce4f83da8dff4b7
Anton <leeeloooda@gmail.com>
2021-04-29 22:43:47 +0300

    add front side

--- frontend/src/Profile/Profile.tsx
+++ frontend/src/Profile/Profile.tsx
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import './Profile.css';
 import NavBar from '../NavBar/NavBar';
+import DragAndDrop from '../DragAndDrop/DragAndDrop';
 import { FORM_ERROR_MESSAGES, serverAddress } from '../constants';
 
 interface IState {
@@ -41,20 +42,7 @@ export default class Profile extends React.Component<IProps, IState> {
         }
         this.handleInputChange = this.handleInputChange.bind(this);
         this.submitForm = this.submitForm.bind(this);
-        this.loadProfile();
-    }
-
-    loadProfile(): void {
-        this.setState({
-            isLoading: true,
-            loadingError: undefined,
-            profile: {},
-            oldPassword: '',
-            newPassword: '',
-            newPasswordRepeat: '',
-            fieldsError: {},
-            newAvatarPath: ''
-        });
+        this.handleFileChoose = this.handleFileChoose.bind(this);
     }
 
     componentDidMount() {
@@ -114,6 +102,10 @@ export default class Profile extends React.Component<IProps, IState> {
         }
     }
 
+    async loadAvatar(file: any): Promise<void> {
+        console.log(file);
+    }
+
     handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
         this.setState(
             { [event.target.name]: event.target.value }
@@ -150,6 +142,20 @@ export default class Profile extends React.Component<IProps, IState> {
         });
     }
 
+    handleFileDrop = (files: any) => {
+        if (files.length == 1 && files[0].type.split('/')[0] === 'image') {
+            this.loadAvatar(files[0]);
+        } else {
+            //TODO: U WANT TOO MUCH SAY IN STATE
+        }
+    }
+
+    async handleFileChoose(event: any) {
+        if (event.target.files.length == 1) {
+            this.loadAvatar(event.target.files[0]);
+        }
+    }
+
     loading(): JSX.Element {
         return (
             <div>loading...</div>
@@ -163,7 +169,7 @@ export default class Profile extends React.Component<IProps, IState> {
     }
 
     profile(): JSX.Element {
-        return (
+        const view = (
             <div className="profile_content">
                 <div className="profile_content_information">
                         <img src={this.state.profile?.avatar} className="profile_content_information_avatar"/>
@@ -202,19 +208,21 @@ export default class Profile extends React.Component<IProps, IState> {
                         </div>
                         <div className="profile_content_change_image">
                             <h3>Change profile image</h3>
-                            <form className="profile_content_change_image_drop">
+                            <DragAndDrop handleFileDrop={this.handleFileDrop}>
                                 Add new profile image
-                                <button className='profile_content_upload_image_btn btn_icon'>
+                                <label htmlFor="file-input" className='profile_content_upload_image_btn btn_icon'>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                         <path d="M0 0h24v24H0z" fill="none"/>
                                         <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
                                     </svg>
-                                </button>
-                            </form>
+                                </label>
+                                <input id="file-input" accept="image/*" type='file' onChange={ this.handleFileChoose }/>
+                            </DragAndDrop>
                         </div>
                     </div>
             </div>
         );
+        return view;
     }
 
     render(): JSX.Element {
