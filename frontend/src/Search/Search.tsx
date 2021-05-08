0212539189a08cd0904021ade2584cd5d9c707e3
Ivan Angelov <angelow.van@yandex.ru>
2021-05-08 23:28:23 +0300

    Restyled Search component

--- frontend/src/Search/Search.tsx
+++ frontend/src/Search/Search.tsx
@@ -1,27 +1,29 @@
 import React from 'react'; 
 import './Search.css';
 import NavBar from '../NavBar/NavBar';
+import viewersIcon from '../assets/viewers-icon.png';
 
-const searchResult = [
-    { avatar: 'https://randomuser.me/api/portraits/women/24.jpg', name: 'Loretta Snyder', views: 3 },
-    { avatar: 'https://randomuser.me/api/portraits/men/88.jpg', name: 'Gordon Matthews', views: 4421 },
-    { avatar: 'https://randomuser.me/api/portraits/men/29.jpg', name: 'Franklin Cook', views: 321 },
-    { avatar: 'https://randomuser.me/api/portraits/women/40.jpg', name: 'Pamela Stewart', views: 412 },
-    { avatar: 'https://randomuser.me/api/portraits/women/50.jpg', name: 'Terri Peterson', views: 421 },
-    { avatar: 'https://randomuser.me/api/portraits/women/61.jpg', name: 'Hilda Reynolds', views: 12 },
-    { avatar: 'https://randomuser.me/api/portraits/women/24.jpg', name: 'Loretta Snyder', views: 3 },
-    { avatar: 'https://randomuser.me/api/portraits/men/88.jpg', name: 'Gordon Matthews', views: 4421 },
-    { avatar: 'https://randomuser.me/api/portraits/men/29.jpg', name: 'Franklin Cook', views: 321 },
-    { avatar: 'https://randomuser.me/api/portraits/women/40.jpg', name: 'Pamela Stewart', views: 412 },
-    { avatar: 'https://randomuser.me/api/portraits/women/50.jpg', name: 'Terri Peterson', views: 421 },
-    { avatar: 'https://randomuser.me/api/portraits/women/61.jpg', name: 'Hilda Reynolds', views: 12 }
-];
-
-interface Streamer {
-    avatar?: string;
-    name?: string;
-    views?: number
+interface StreamItem {
+    avatar: string;
+    presenterName: string;
+    streamName: string;
+    viewersCount: number
 }
+
+const searchResult: StreamItem[] = [
+    { avatar: 'https://randomuser.me/api/portraits/women/24.jpg', presenterName: 'Loretta Snyder Loretta Snyder Loretta Snyder Loretta Snyder', streamName: 'Dancing', viewersCount: 3 },
+    { avatar: 'https://randomuser.me/api/portraits/men/88.jpg', presenterName: 'Gordon Matthews', streamName: 'Just Chatting', viewersCount: 4421 },
+    { avatar: 'https://randomuser.me/api/portraits/men/29.jpg', presenterName: 'Franklin Cook', streamName: 'Sunday vlog', viewersCount: 321 },
+    { avatar: 'https://randomuser.me/api/portraits/women/40.jpg', presenterName: 'Pamela Stewart', streamName: 'Films', viewersCount: 412 },
+    { avatar: 'https://randomuser.me/api/portraits/women/50.jpg', presenterName: 'Terri Peterson', streamName: 'WWWWWWWWWWWWWWWWWWWWWWWWWW WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW', viewersCount: 421 },
+    { avatar: 'https://randomuser.me/api/portraits/women/61.jpg', presenterName: 'Hilda Reynolds', streamName: 'Watching The International 3 replays Watching The International 3 replays Watching The International 3 replays', viewersCount: 12 },
+    { avatar: 'https://randomuser.me/api/portraits/women/24.jpg', presenterName: 'Loretta Snyder', streamName: 'Sample name', viewersCount: 3 },
+    { avatar: 'https://randomuser.me/api/portraits/men/88.jpg', presenterName: 'Gordon Matthews', streamName: 'Rocket League Grand Champion', viewersCount: 4421 },
+    { avatar: 'https://randomuser.me/api/portraits/men/29.jpg', presenterName: 'Franklin Cook', streamName: 'Studying Python together', viewersCount: 321 },
+    { avatar: 'https://randomuser.me/api/portraits/women/40.jpg', presenterName: 'Pamela Stewart', streamName: 'Python sucks', viewersCount: 412 },
+    { avatar: 'https://randomuser.me/api/portraits/women/50.jpg', presenterName: 'Terri Peterson', streamName: 'Raquel is talking', viewersCount: 421 },
+    { avatar: 'https://randomuser.me/api/portraits/women/61.jpg', presenterName: 'Hilda Reynolds', streamName: 'Hilda #2', viewersCount: 12 }
+];
 
 interface IProps {
     showNotification: (type: 'info' | 'error' | 'success', text: string, notificationTimeout?: number) => void;
@@ -32,11 +34,10 @@ interface IState {
     isInitial?: boolean;
     isLoading?: boolean;
     errorLoading?: Error;
-    data: Streamer[];
+    data: StreamItem[];
 }
 
 export default class Search extends React.Component<IProps, IState> {
-
     constructor(props: IProps) {
         super(props);
         this.state = {
@@ -46,13 +47,11 @@ export default class Search extends React.Component<IProps, IState> {
             errorLoading: undefined,
             data: []
         };
-        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleQueryInputChange = this.handleQueryInputChange.bind(this);
     }
 
-    handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
-        this.setState({ 
-            searchQuery: event.target.value 
-        });
+    handleQueryInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
+        this.setState({ searchQuery: event.target.value });
     }
 
     onSearchClick() {
@@ -88,15 +87,23 @@ export default class Search extends React.Component<IProps, IState> {
     }
 
     streams(): JSX.Element {
-        const streams = this.state.data.map((item: any, index: number) =>
+        const streams = this.state.data.map((item: StreamItem, index: number) =>
             <div key={index} className='Search-stream_item'>
-                <img src={item.avatar} className='Search-stream_item_avatar'/>
-                {item.name}
+                <img src={item.avatar} className='Search-stream_item-avatar'/>
+                <div className='Search-stream_item-stream_info_block'>
+                    <div className='Search-stream_item-stream_name_block'>
+                        <span title={item.presenterName}>{item.presenterName}</span>
+                        <h3 title={item.streamName}>{item.streamName}</h3>
+                    </div>
+                    <div className='Search-stream_item-viewers_count_block'>
+                        <span>{item.viewersCount}</span>
+                        <img className='Search-stream_item-viewers_icon' src={viewersIcon}/>
+                    </div>
+                </div>
             </div>
         );
         return (
             <div className='Search-stream_list'>
-                { 'Found ' + this.state.data.length + ' ' + (this.state.data.length === 1 ? 'stream' : 'streams') }
                 {streams}
             </div>
         );
@@ -107,20 +114,20 @@ export default class Search extends React.Component<IProps, IState> {
             <div className='Search-component'>
                 <NavBar currentTab={0} showNotification={this.props.showNotification}/>
                 <div className='Search-search_container'>
-                    <span className='Search-search_title'>Search by author or name</span>
-                    <div className='Search-search_inputs'>
-                        <input type='text' name='searchQuery'
-                            value={this.state.searchQuery}
-                            onChange={this.handleInputChange}/>
-                        <button className='Search-search_btn' onClick={ () => this.onSearchClick() }>Search</button>
+                    <h2>Search by author or name</h2>
+                    <div className='Search-search_input_block'>
+                        <input type='text' value={this.state.searchQuery}
+                               onChange={this.handleQueryInputChange}/>
+                        <button className='common_button' onClick={ () => this.onSearchClick() }>Search</button>
                     </div>
                 </div>
-                <div className='Search-search_container_results'>
-                    { 
-                        this.state.isLoading === true ? this.loading() : 
-                            this.state.errorLoading !== undefined ? this.error() : 
-                            this.state.isInitial ? <div/> : this.streams() 
-                    }
+                <div className='Search-search_results_container'>
+                    {/*{ */}
+                    {/*    this.state.isLoading === true ? this.loading() : */}
+                    {/*        this.state.errorLoading !== undefined ? this.error() : */}
+                    {/*        this.state.isInitial ? <div/> : this.streams() */}
+                    {/*}*/}
+                    {this.streams()}
                 </div>
             </div>
         );
