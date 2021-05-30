9873d9fd57100b070043dcf7f5d9da2684304b6e
Ivan Angelov <angelow.van@yandex.ru>
2021-05-30 20:07:37 +0300

    Added dockerfile for backend and corresponding service in docker-compose.yml

--- backend/src/config/constants.ts
+++ backend/src/config/constants.ts
@@ -2,8 +2,8 @@ import path from 'path';
 
 export const PORT = process.env.PORT || 4000;
 export const certDir = path.resolve('cert.dev');
-export const wsUri = 'ws://localhost:8888/kurento';
-export const mongoUri = 'mongodb://lekcio:s3cret_mongo@localhost:27017/lekcio';
+export const wsUri = 'ws://127.0.0.1:8888/kurento';
+export const mongoUri = 'mongodb://lekcio:s3cret_mongo@127.0.0.1:27017/lekcio';
 export const jwtConfig = {
     secret: 'Gszh0e4MCCseLY0umcmlWE32GcymUUKExKrNTAnejmHwbg4NaOU8oIs+x+PWDdBc',
     algorihtm: 'HS256',
